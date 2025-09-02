b9ac01351600bf8e8ee93d3d2b6de99aad4e2d40
Jarod Smith <lbriones.dev@gmail.com>
2025-09-02 09:12:44 -0400

    Upd/ Implementa CSS y mejora diseño de productos page

--- assets/productos.js
+++ assets/productos.js
@@ -21,11 +21,11 @@ document.addEventListener('DOMContentLoaded', function () {
           <div class="col-12 col-md-6 col-lg-4 d-flex">
             <div class="card mx-auto shadow-sm" style="width: 60%; display: flex; flex-direction: column; flex: 1;">
               <img src="./assets/img/logo.jpg" class="card-img-top" alt="${producto.nombre}">
-              <div class="card-body d-flex flex-column flex-grow-1">
-                <h5 class="card-title fs-6">${producto.nombre}</h5>
-                <p class="card-text fs-2 fw-bold">Precio: ${formatoMoneda.format(producto.precio)}</p>
-                <p class="flex-grow-1 text-center">${producto.descripcion || ''}</p>
-                <a href="#" class="btn btnBrown mt-auto">Go somewhere</a>
+              <div class="card-body d-flex flex-column flex-grow-1 text-center">
+                <h5 class="card-title fs-3">${producto.nombre}</h5>
+                <p class="card-text fs-2 fw-bold producto__precio">Precio: <span id="producto__precio--moneda">${formatoMoneda.format(producto.precio)}</span></p>
+                <p class="flex-grow-1 producto__descripcion">${producto.descripcion || ''}</p>
+                <a href="#" class="btn btnBrown mt-auto fs-5"><i class="fa-solid fa-cart-plus"></i> Agregar</a>
               </div>
             </div>
           </div>
